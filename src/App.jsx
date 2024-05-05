e8f6711c3c8df4f74d96dee7ceeb78f822c56188
Dinny Hendricks <dinhen@lwg-3t7k1f3.lenslogistics.int>
2024-05-05 14:00:12 +0200

    removed pages refs from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import Artifacts from './Pages/Artifacts'
-import Essence from './Pages/Essence'
-import Home from './Pages/Home'
-import ScrumTeam from './Pages/ScrumTeam'
-import SprintEvents from './Pages/SprintEvents'
+
 import Layout from './Pages/Layout'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
