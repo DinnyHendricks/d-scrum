5877e2db0141ab07a90ae14f8d67c97e0114354a
Dinny Hendricks <dinhen@lwg-3t7k1f3.lenslogistics.int>
2024-05-05 14:24:52 +0200

    order

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import Artifacts from './Pages/Artifacts'
-import Essence from './Pages/Essence'
+import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Essence from './Pages/Essence'
 import ScrumTeam from './Pages/ScrumTeam'
 import SprintEvents from './Pages/SprintEvents'
+import Artifacts from './Pages/Artifacts'
 import Layout from './Pages/Layout'
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
       <Routes>
-        
-          
-          <Route path='/essence' element={<Essence />}/>
-          <Route path='/scrum-team' element={<ScrumTeam />}/>
-          <Route path='/events' element={<SprintEvents />}/>
-          <Route path='/artifacts' element={<Artifacts />}/>
-          <Route path='/' element={<Home />}/>
-
+        <Route path='/' element={<Home />} />
+        <Route path='/essence' element={<Essence />} />
+        <Route path='/scrum-team' element={<ScrumTeam />} />
+        <Route path='/events' element={<SprintEvents />} />
+        <Route path='/artifacts' element={<Artifacts />} />
       </Routes>
     </>
   )
 }
 
-export default App
+export default App;
