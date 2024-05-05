ab8880e2d03042dd3f9ebcd1175d68057136fbac
Dinny Hendricks <dinhen@lwg-3t7k1f3.lenslogistics.int>
2024-05-05 15:01:25 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Essence from './Pages/Essence'
-import ScrumTeam from './Pages/ScrumTeam'
-import SprintEvents from './Pages/SprintEvents'
-import Artifacts from './Pages/Artifacts'
-import Layout from './Pages/Layout'
+import Home from './pages/Home'
+import Essence from './pages/Essence'
+import ScrumTeam from './pages/ScrumTeam'
+import SprintEvents from './pages/SprintEvents'
+import Artifacts from './pages/Artifacts'
+import Layout from './pages/Layout'
 import './App.css'
 
 function App() {
