87cb8eb9da63406ebfb6568af71f0170c3cccb03
Dinny Hendricks <dinhen@lwg-3t7k1f3.lenslogistics.int>
2024-05-05 14:53:39 +0200

    layout back

--- src/Pages/Layout/index.jsx
+++ src/Pages/Layout/index.jsx
@@ -1,28 +1,25 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { Menu } from "lucide-react";
-import { useState, useRef } from "react";
-import styles from './Layout.module.css'
+import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/artifacts">artifacts</Link>
+          </li>
+          <li>
+            <Link to="/essence">essence</Link>
+          </li>
+        </ul>
+      </nav>
 
-
-    return(
-        <>
-        <div className={styles.navbar}>
-            <Menu onClick={handleClick} ref={navItemsContainer} className={styles.hamburger}/>
-                    {showNavItems &&
-                        <div className={styles.navigation}>
-                            <NavLink className={({ isActive}) => isActive ?  styles.active : ''} to='/'>Home</NavLink>
-                            <NavLink className={({ isActive}) => isActive ?  styles.active : ''} to='essence'>The Essence of Scrum</NavLink>
-                            <NavLink className={({ isActive}) => isActive ?  styles.active : ''} to='scrum-team'>Scrum Team</NavLink>
-                            <NavLink className={({ isActive}) => isActive ?  styles.active : ''} to='events'>Sprint Events</NavLink>
-                            <NavLink className={({ isActive}) => isActive ?  styles.active : ''} to='artifacts'>Three Artifacts</NavLink>
-                        </div>
-                    }
-        </div>
-        <Outlet />
-        </>
-    )
-}
+      <Outlet />
+    </>
+  )
+};
 
 export default Layout;
